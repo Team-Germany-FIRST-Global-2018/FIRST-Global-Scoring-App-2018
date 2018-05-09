1557d7233503698018315909f3e47a74fc89a642
Scroogeee <30441849+Scroogeee@users.noreply.github.com>
2018-05-09 18:43:25 +0200

    All time based scoring works now. However the score itself is not updated yet.

--- app/src/main/java/de/firstglobalgermany/fgcscoringapp/MainActivity.java
+++ app/src/main/java/de/firstglobalgermany/fgcscoringapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
@@ -16,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
 	long time;
 
 	TextView timeView;
+	Button[] solarButtons = new Button[5];
+	TextView[] solarViews = new TextView[5];
+	int[] solarTime = new int[5];
+
+	TextView windView;
+	Button windButton;
+	int windTime = 0;
+
+	TextView reactionView;
+	Button reactionButton;
+	int reactionTime;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,27 +35,98 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 		timeView = findViewById(R.id.vTime);
+		getSolars();
+		windView = findViewById(R.id.vWind);
+		windButton = findViewById(R.id.bWind);
+		reactionView = findViewById(R.id.vReaction);
+		reactionButton = findViewById(R.id.bReaction);
+
 		countdown = createTimer();
 	}
 
-	public CountDownTimer createTimer(){
+	private CountDownTimer createTimer(){
 		return new CountDownTimer(150000, 1000) {
 			@Override
 			public void onTick(long l) {
 				int minutes = (int)Math.floor(l/1000/60);
 				int seconds = (int)(l-minutes*1000*60)/1000;
+				time = l;
 				timeView.setText(Integer.toString(minutes)+":"+Integer.toString(seconds));
 			}
 
 			@Override
 			public void onFinish() {
+				timeView.setText("0:00");
 				timeView.setTextColor(Color.RED);
 			}
 		};
 	}
 
+	private void getSolars(){
+		solarButtons[0] = findViewById(R.id.bSolar0);
+		solarButtons[1] = findViewById(R.id.bSolar1);
+		solarButtons[2] = findViewById(R.id.bSolar2);
+		solarButtons[3] = findViewById(R.id.bSolar3);
+		solarButtons[4] = findViewById(R.id.bSolar4);
+
+		solarViews[0] = findViewById(R.id.vSolar0);
+		solarViews[1] = findViewById(R.id.vSolar1);
+		solarViews[2] = findViewById(R.id.vSolar2);
+		solarViews[3] = findViewById(R.id.vSolar3);
+		solarViews[4] = findViewById(R.id.vSolar4);
+	}
+
+	private String getTimeString(long millisec){
+		int minutes = (int)Math.floor(millisec/1000/60);
+		int seconds = (int)(millisec - minutes*1000*60)/1000;
+		String addition = "";
+		if(seconds < 10){
+			addition = "0";
+		}
+		return (Integer.toString(minutes)+":"+addition+Integer.toString(seconds));
+	}
 	public void start(View v){
 		countdown.start();
+	}
+
+	public void scoreSolar(View v){
+		int solarNum = 0;
+		for(int i = 0; i<5; i++){
+			if(v == solarButtons[i]){solarNum = i;}
+		}
+		if(solarTime[solarNum] == 0) {
+			solarButtons[solarNum].setText("Unscore");
+			solarViews[solarNum].setText(getTimeString(time));
+			solarTime[solarNum] = (int)time/1000;
+		} else {
+			solarButtons[solarNum].setText("Solar");
+			solarViews[solarNum].setText("0:00");
+			solarTime[solarNum] = 0;
+		}
+	}
+
+	public void scoreWind(View v){
+		if(windTime == 0){
+			windButton.setText("Unscore");
+			windView.setText(getTimeString(time));
+			windTime = (int)time/1000;
+		} else {
+			windButton.setText("Wind");
+			windView.setText("0:00");
+			windTime = 0;
+		}
+	}
+
+	public void scoreReaction(View v){
+		if(reactionTime == 0){
+			reactionButton.setText("Unscore");
+			reactionView.setText(getTimeString(time));
+			reactionTime = (int)time/1000;
+		} else {
+			reactionButton.setText("Reaction");
+			reactionView.setText("0:00");
+			reactionTime = 0;
+		}
 	}
 	/*
 	int solarPanels = 0;
